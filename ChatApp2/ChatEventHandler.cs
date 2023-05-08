b9d0a54bfa766bf8236eff40d22c15d421ae2a47
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-05-08 17:02:47 +0100

    pub/sub relationship

---
+++ ChatApp2/ChatEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Event_bus_App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp2
+{
+    public class ChatEventHandler : IEventHandler
+    {
+        public void Handle<T>(T @event) where T : Event
+        {
+           Console.WriteLine(@event.Message);
+        }
+    }
+}
---
+++ ChatApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿using Event_bus_App;
+
+IEventBus _eventBus = new EventBus();
+
+_eventBus.Subscribe<Event>
