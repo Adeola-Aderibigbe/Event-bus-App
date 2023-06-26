55ae2632ea23a56cf5fd7529794827ab74742acf
Aderibigbe Adeola <115571361+Adeola-Aderibigbe@users.noreply.github.com>
2023-06-26 11:56:42 +0100

    Add background service to send a message

---
+++ BackgroundService/EventFactory.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace BackgroundService
+{
+    public static class EventFactory
+    {
+        public static void CreateConnection(string message)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "EmailService",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: true,
+                                 arguments: null); 
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "EmailService",
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent {0}",message);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BackgroundService/Program.cs
@@ -0,0 +1,22 @@
+﻿using BackgroundService;
+using Hangfire;
+
+GlobalConfiguration.Configuration
+    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+    .UseSimpleAssemblyNameTypeSerializer()
+    .UseRecommendedSerializerSettings()
+    .UseSqlServerStorage("Server = (localdb)\\MSSQLLocalDB;Database=EmailService; Integrated Security=True;");
+
+
+BackgroundJob.Enqueue(() => Console.WriteLine("Hello from Email service"));
+
+using (new BackgroundJobServer())
+{
+    EventFactory.CreateConnection("Start publishing event");
+    Console.ReadKey();
+}
+
+//RecurringJob.AddOrUpdate(() => Console.WriteLine("Recurring job completed successfully!"), Cron.Minutely);
+
+
+
